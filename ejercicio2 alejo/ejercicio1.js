871618713febbd6dcb0088f85e684417ef5fcb7f
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-02-16 07:26:14 -0500

    1

--- ejercicio2 alejo/ejercicio1.js
+++ ejercicio2 alejo/ejercicio1.js
@@ -1,1 +1,20 @@
+function saludar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (nombre === "Tomas") {
+                resolve("Hola Tomas, bienvenido");
+            } else {
+                reject("No te conozco");
+            }
+        }, 2000);
+    });
+}
 
+// Consumir la promesa
+saludar("Tomas")
+    .then((mensaje) => {
+        console.log(mensaje);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
